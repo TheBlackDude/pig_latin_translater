4402cf24bc8b88e7843de602717b5f1d13472411
Ousmane Seidy Diallo <ousmane.mudu@gmail.com>
2019-06-09 18:11:15 -0400

    feat: add translation and api endpoint.

---
+++ app.py
@@ -0,0 +1,20 @@
+#!flask/bin/python
+from flask import Flask, jsonify, request, render_template
+from translate import translate_word
+
+app = Flask(__name__)
+
+@app.route('/api/v1/translate', methods=['POST'])
+def translater():
+    if not request.json or not 'word' in request.json:
+        abort(400)
+    word = request.json.get('word')
+    translated = " ".join(translate_word(word) for word in word.split())
+    return jsonify({'word': translated}), 201
+
+@app.route('/')
+def index():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ translate.py
@@ -0,0 +1,18 @@
+
+def startwith_vowel(word):
+    for l in 'aeiou':
+        if word.startswith(l): return True
+    return False
+
+def translate_word(word):
+    pyg = 'ay'
+
+    if word and word.isalpha():
+        title = word.istitle()
+        word = word.lower()
+        if startwith_vowel(word):
+            if title: return word.title() + pyg
+            return word + pyg
+        if title: return word[1:].title() + word[0] + pyg
+        return word[1:] + word[0] + pyg
+    return word
